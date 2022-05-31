7b017dd491554ecca44d94c0044640c6790aa6eb
Joey Vanhalst <joey.vanhalst@skyline.be>
2022-05-31 13:47:21 +0200

    updated assembly name
    Change-Id: I2e25e98e99f2cdb94ba8f7806455087171f5490b

--- EVS IPD-VIA/Properties/AssemblyInfo.cs
+++ EVS IPD-VIA/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SLC.CLib.EVS IPD-VIA")]
-[assembly: AssemblyDescription("SLC.CLib.EVS IPD-VIA")]
+[assembly: AssemblyTitle("SLC.CLib.EVS_IPD-VIA")]
+[assembly: AssemblyDescription("SLC.CLib.EVS_IPD-VIA")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skyline Communications")]
 [assembly: AssemblyProduct("DataMiner")]
